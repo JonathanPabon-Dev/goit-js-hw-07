3a21c2b4ba99fd1a49dfb16d3d057fa1760d54c1
JonathanPabon-Dev <jonathanpabon.dev@gmail.com>
2024-01-22 13:56:14 -0500

    Se agrega la solución al repositorio

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+        )
+        .join(" ");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
+
+galleryContainer.addEventListener("click", openModal);
+
+function openModal(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+	    <div class="modal">
+        <img src="${e.target.dataset.source}" alt="${e.target.alt}">
+      </div>
+    `);
+
+    instance.show();
+
+    document.addEventListener("keydown", closeModal);
+
+    function closeModal(e) {
+        if (e.code !== "Escape" && basicLightbox.visible()) {
+            return;
+        }
+        instance.close();
+        document.removeEventListener("keydown", closeModal);
+    }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+        )
+        .join(" ");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
+
+galleryContainer.addEventListener("click", openModal);
+
+function openModal(e) {
+    e.preventDefault();
+}
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: "250ms",
+});
